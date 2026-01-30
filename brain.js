83124f05c5218bc42d1bb8109d67dfa348dbe735
QUILLIEC-Matisse-24019562 <163044607+QUILLIEC-Matisse-24019562@users.noreply.github.com>
2026-01-30 12:07:48 +0100

    add tabs

---
+++ brain.js
@@ -0,0 +1,176 @@
+
+var A_fish = {} ; 
+var I_val_min = -10;
+var I_val_max = 40;
+for (let I_index = 0 ; I_index < 20 ; I_index++){
+	A_fish[I_index] = Math.floor(Math.random() * (I_val_max - I_val_min) + I_val_min);
+}
+
+function check_color(){
+    let I_value = A_fish[I_caramel];
+    if (I_value <= 0){
+        O_element.setAttribute("class", "un");
+        O_element2.textContent = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
+    }
+    else if (I_value <= 20){
+        O_element.setAttribute("class", "deux");
+        O_element2.textContent = "ㅤ";
+    }
+    else if (I_value <= 30){
+        O_element.setAttribute("class", "tres");
+        O_element2.textContent = "ㅤ";
+    }
+    else if (I_value <= 40){
+        O_element.setAttribute("class", "four");
+        O_element2.textContent = "Caliente ! Vamos a la playa, ho hoho hoho !!";
+    }
+}
+
+let O_element = document.getElementById("case");
+var I_caramel = 0;
+let O_element2 = document.getElementById("funny_sentence");
+
+function change_value(){
+    O_element.textContent = A_fish[I_caramel];
+    check_color();
+    I_caramel = (I_caramel + 1) % 20;
+}
+
+setInterval(change_value, 2000);
+
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *
+ *   File:   tabs-manual.js
+ *
+ *   Desc:   Tablist widget that implements ARIA Authoring Practices
+ */
+
+'use strict';
+
+class TabsManual {
+  constructor(groupNode) {
+    this.tablistNode = groupNode;
+
+    this.tabs = [];
+
+    this.firstTab = null;
+    this.lastTab = null;
+
+    this.tabs = Array.from(this.tablistNode.querySelectorAll('[role=tab]'));
+    this.tabpanels = [];
+
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      var tabpanel = document.getElementById(tab.getAttribute('aria-controls'));
+
+      tab.tabIndex = -1;
+      tab.setAttribute('aria-selected', 'false');
+      this.tabpanels.push(tabpanel);
+
+      tab.addEventListener('keydown', this.onKeydown.bind(this));
+      tab.addEventListener('click', this.onClick.bind(this));
+
+      if (!this.firstTab) {
+        this.firstTab = tab;
+      }
+      this.lastTab = tab;
+    }
+
+    this.setSelectedTab(this.firstTab);
+  }
+
+  setSelectedTab(currentTab) {
+    for (var i = 0; i < this.tabs.length; i += 1) {
+      var tab = this.tabs[i];
+      if (currentTab === tab) {
+        tab.setAttribute('aria-selected', 'true');
+        tab.removeAttribute('tabindex');
+        this.tabpanels[i].classList.remove('is-hidden');
+      } else {
+        tab.setAttribute('aria-selected', 'false');
+        tab.tabIndex = -1;
+        this.tabpanels[i].classList.add('is-hidden');
+      }
+    }
+  }
+
+  moveFocusToTab(currentTab) {
+    currentTab.focus();
+  }
+
+  moveFocusToPreviousTab(currentTab) {
+    var index;
+
+    if (currentTab === this.firstTab) {
+      this.moveFocusToTab(this.lastTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index - 1]);
+    }
+  }
+
+  moveFocusToNextTab(currentTab) {
+    var index;
+
+    if (currentTab === this.lastTab) {
+      this.moveFocusToTab(this.firstTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index + 1]);
+    }
+  }
+
+  /* EVENT HANDLERS */
+
+  onKeydown(event) {
+    var tgt = event.currentTarget,
+      flag = false;
+
+    switch (event.key) {
+      case 'ArrowLeft':
+        this.moveFocusToPreviousTab(tgt);
+        flag = true;
+        break;
+
+      case 'ArrowRight':
+        this.moveFocusToNextTab(tgt);
+        flag = true;
+        break;
+
+      case 'Home':
+        this.moveFocusToTab(this.firstTab);
+        flag = true;
+        break;
+
+      case 'End':
+        this.moveFocusToTab(this.lastTab);
+        flag = true;
+        break;
+
+      default:
+        break;
+    }
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  // Since this example uses buttons for the tabs, the click onr also is activated
+  // with the space and enter keys
+  onClick(event) {
+    this.setSelectedTab(event.currentTarget);
+  }
+}
+
+// Initialize tablist
+
+window.addEventListener('load', function () {
+  var tablists = document.querySelectorAll('[role=tablist].manual');
+  for (var i = 0; i < tablists.length; i++) {
+    new TabsManual(tablists[i]);
+  }
+});
